fc7f7aea3454c7c163b50692c9edb61f0ddece0d
natasavatres <natasavatres@gmail.com>
2016-03-26 16:20:09 +0100

    Interfejs menjacnicaInterfejs implementiran u klasi Menjacnica.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,9 +1,30 @@
 package menjacnica;
 
+import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
-public class Menjacnica {
+import specifikacijainterfejsa.menjacnicaInterfejs;
+
+public class Menjacnica implements menjacnicaInterfejs{
 
 	LinkedList<Valuta> menjacnica = new LinkedList<Valuta>();
+
+	@Override
+	public void dodajKurs(Valuta valuta) {
+		
+		
+	}
+
+	@Override
+	public void obrisiKurs(String nazivKursa, GregorianCalendar datum) {
+	
+		
+	}
+
+	@Override
+	public Kurs pronadjiKurs(String nazivKursa, GregorianCalendar datum) {
+		
+		return null;
+	}
 
 }
