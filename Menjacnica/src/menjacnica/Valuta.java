4500de1fdc18376dae259826c6624f5371fa3cfc
natasavatres <natasavatres@gmail.com>
2016-03-26 16:37:52 +0100

    Dopunjene su metode koje implementira klasa Menjacnica.

--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -7,7 +7,7 @@ public class Valuta {
 	private String naziv;
 	private String skraceniNaziv;
 
-	private LinkedList<Kurs> kurs = new LinkedList<Kurs>();
+	LinkedList<Kurs> kurs = new LinkedList<Kurs>();
 
 	public String getNaziv() {
 		return naziv;
